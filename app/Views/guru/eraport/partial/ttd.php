11ec8daafe01c2fe6b22489d7ab263efc252e16d
yudistjr <wahyudistiragunners@gmail.com>
2023-06-17 03:12:41 +0700

    Fix print e raport

--- app/Views/guru/eraport/partial/ttd.php
+++ app/Views/guru/eraport/partial/ttd.php
@@ -1,4 +1,4 @@
-<div class="float-right" style="margin-top: 200px;">
+<div class="float-right" style="margin-top: 50px;">
     <p>Kebumen, <?= date("d") . ' ' . $listBulan[(int)date("m")] . ' ' . date("Y")  ?></p>
     <p class="text-center">Wali Kelas</p>
     <p class="font-weight-bold text-center text-uppercase" style="margin-top: 100px;"><?= $dtKelas['nama'] ?></p>
