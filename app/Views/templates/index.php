7cc87781a582a2ec732ffba062adbcd14f6e4897
yudistjr <wahyudistiragunners@gmail.com>
2023-06-19 14:45:00 +0700

    Fix img preview

--- app/Views/templates/index.php
+++ app/Views/templates/index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <?php header("Content-Security-Policy: img-src 'self' blob: data: https://siasmarigo.online/"); ?>
+    <?php header("Content-Security-Policy: img-src 'self' blob: data: " . base_url()); ?>
 
     <title><?= $title ?> | <?= $sub_title ?></title>
 
